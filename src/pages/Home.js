4323add1ff20506e083e335507962824926f9b37
arantza-yes <yeseniaarantza@gmail.com>
2021-07-02 21:40:59 -0500

    filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import getData from '../utils/getData';
 
+// let press = window.getElementById('#text');
+const press = null || document.getElementById('text');
+
+press.addEventListener('keypress', (e) => {
+  console.log(e.target.value);
+});
+
 const Home = async () => {
   const characters = await getData();
+
   const view = `
     <div class="">
       <div class="h-full mt-5 container mx-auto grid grid-cols-3 gap-4">
