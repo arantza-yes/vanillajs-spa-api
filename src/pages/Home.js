a48b26c93dfbb4a793d00550cdfd39530991a45e
arantza-yes <yeseniaarantza@gmail.com>
2021-07-08 14:16:16 -0500

    filter 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,100 @@
 import getData from '../utils/getData';
 
 // let press = window.getElementById('#text');
-
+// let dato;
+// function filtra(id) {
+//   return (id[1] = dato);
+// }
+let filtrar;
 const Home = async () => {
   const characters = await getData();
+  const boton = document.getElementById('bt');
 
-  let filtrar = characters.results.filter((filtra) => (filtra.length = [0]));
-  console.log(filtrar);
+  boton.addEventListener('click', () => {
+    // debugger;
 
-  const view = `
-    <div class="">
-      <div class="h-full mt-5 container mx-auto grid grid-cols-3 gap-4">
-      ${characters.results
-        .map(
-          (character) => `
-      <article class="mx-auto">
-        <a href="#/${character.id}/">
-          <img src="${character.image}" alt="${character.name}">
-          <h2>${character.name}</h2>
-        </a>
-      </article>
-      `
-        )
-        .join('')}
+    // dato = e.target.value;
+    // FILTRAR TIENE TODO
+    filtrar = characters.results.filter(filtra);
+    console.log(filtrar);
+
+    function filtra(id) {
+      let press = document
+        .getElementById('text')
+        .value.toLocaleLowerCase()
+        .slice(0, 4);
+      let nom = id.name.toLocaleLowerCase().slice(0, 4);
+      let ob = false;
+
+      if (nom == press) {
+        ob = true;
+      }
+      return ob;
+    }
+    if (filtrar.length === 0) {
+      debugger;
+      return `
+      <div class="">
+        <div class="h-full mt-5 container mx-auto grid grid-cols-3 gap-4">
+        ${filtrar
+          .map(
+            (character) => `
+        <article class="mx-auto">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+        `
+          )
+          .join('')}
+        </div>
       </div>
-    </div>
-  `;
+    `;
+    }
+    return `
+      <div class="">
+        <div class="h-full mt-5 container mx-auto grid grid-cols-3 gap-4">
+        ${filtrar
+          .map(
+            (character) => `
+        <article class="mx-auto">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+        `
+          )
+          .join('')}
+        </div>
+      </div>
+    `;
 
-  return view;
+    // return  filtrar;
+
+    // return filtrar;
+  });
+
+  //   return `
+  //   <div class="">
+  //     <div class="h-full mt-5 container mx-auto grid grid-cols-3 gap-4">
+  //     ${characters.results
+  //       .map(
+  //         (character) => `
+  //     <article class="mx-auto">
+  //       <a href="#/${character.id}/">
+  //         <img src="${character.image}" alt="${character.name}">
+  //         <h2>${character.name}</h2>
+  //       </a>
+  //     </article>
+  //     `
+  //       )
+  //       .join('')}
+  //     </div>
+  //   </div>
+  // `;
+  // return view;
 };
-
-const press = null || document.getElementById('text');
-
-press.addEventListener('keypress', (e) => {
-  console.log(e.target.value);
-});
 
 export default Home;
